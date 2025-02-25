ff92f85c678a5ff2f88d985b614933fb06ecadb8
Patipat Chewprecha <patipat.che@mahidol.edu>
2025-02-25 10:37:35 +0700

    feat: rename Hub to Sharing

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'Knowledge Hub',
+      title: 'Knowledge Sharing',
       logo: {
         alt: 'iNT Logo',
         src: 'img/cropped-iNT_Logo-180x180.png',
